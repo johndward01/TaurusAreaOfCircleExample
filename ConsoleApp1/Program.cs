751025b39c8fbe4233b59ef3552cc5ded8373134
johndward01 <johndward01@gmail.com>
2022-12-07 15:55:26 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter the radius: ");
+        double radius = double.Parse(Console.ReadLine());
+        var area = AreaOfCircle(radius);
+        Console.WriteLine($"The area of the circle is: {area}");
+    }
+
+    public static double AreaOfCircle(double radius)
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+}
+
+
+
+
+
+
+
+
